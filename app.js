b3bea97f33e3e4feb805d1a98c08cd9fcd3899b2
Addan Shadab <addan_s@Addans-MBP.home>
2020-07-06 21:49:02 -0400

    Added deal method to deck class

--- app.js
+++ app.js
@@ -15,27 +15,31 @@ console.log("Working...");
         create_deck() 
         {
 
-            let card = (suit, value) => 
+            let card = (suit, value,) => 
             {
             this.suit = suit;
-            this.value = value;
+            this.value = value; 
             this.name = value + ' of ' + suit;
 
-            return this.name;
+            return value;
             }
 
             let values = ['2','3','4','5','6','7','8','9','10','J','Q','K','A']; 
             let suits = ['Hearts', 'Spades', 'Diamonds', 'Clubs'];
         
-            for(let i = 0; i <  suits.length; i++)
+            
+             for(let i = 0; i <  suits.length; i++)
             {
                 for(let j = 0; j < values.length; j++)
-                {
-                this.deck.push(card(suits[i], values[j])); 
+                {                    
+                    this.deck.push(card(suits[i], values[j])); 
                 }
 
             }
 
+
+
+            
 
         }
 
@@ -69,8 +73,44 @@ console.log("Working...");
         }
 
 
+        deal()
+        {
+            let dealt_card = this.deck.shift(); 
+            this.cards_dealt.push(dealt_card); 
+            return dealt_card; 
+        }
+
+
     }
 
     deck1 = new Deck; 
     deck1.create_deck();
-    deck1.shuffle();
+    deck1.print_deck();
+    console.log("First Card: " + deck1.deck[0]); 
+    
+
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - Opponent Class - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+
+    class Opponent {
+        constructor() 
+        {
+            this.hand = [];
+            this.funds = 5000;  
+        }
+    }
+
+
+
+
+
+
+
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - Function for adding to pot - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+
+    function roundAction() {
+
+    }
